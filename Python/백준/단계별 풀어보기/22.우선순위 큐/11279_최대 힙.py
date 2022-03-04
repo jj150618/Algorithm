41e7a6b6ca907f968f7f827e1abcc6a303c8d10b
jj150618 <gimhun0@gmail.com>
2022-03-04 23:53:12 +0900

    add 우선순위큐

---
+++ Python/백준/단계별 풀어보기/22.우선순위 큐/11279_최대 힙.py
@@ -0,0 +1,19 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+cnt = int(input())
+
+heap = []
+
+while cnt:
+    a = int(input())
+    if a > 0:
+        heapq.heappush(heap, -a)
+    else:
+        if len(heap) == 0:
+            print(0)
+        else:
+            b = heapq.heappop(heap)
+            print(-b)
+    cnt -= 1
